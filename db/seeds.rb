44fafe76e171eb2b0a7cd7232ae41d4750914121
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2017-05-13 17:31:34 -0400

    some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ParsedUrl.create(url: "http://lim.global")
+ParsedUrl.create(url: "http://google.com")
